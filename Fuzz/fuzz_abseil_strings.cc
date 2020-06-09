3a7e322ec45f7b954d25955e4629cf06855ef047
Kabeer Seth <kabeerseth@google.com>
2020-06-09 13:11:48 +0000

    Fixing errors

--- Fuzz/fuzz_abseil_strings.cc
+++ Fuzz/fuzz_abseil_strings.cc
@@ -4,9 +4,9 @@
 #include "../absl/strings/str_split.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-	std::string str(reinterpret_cast<const char*>(data), size);
+	std::string str = (reinterpret_cast<const char*>(data), size);
 	std::vector<std::string> v = absl::StrSplit(str, ',');
-	std::string str = absl::StrJoin(v, "-");
+	str = absl::StrJoin(v, "-");
 
 	return 0;
 }
