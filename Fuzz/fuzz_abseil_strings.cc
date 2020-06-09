f28f2fd1d5fa5be94c4902c2f3b051eaf2259ec3
Kabeer Seth <kabeerseth@google.com>
2020-06-09 12:23:01 +0000

    Adding skeleton of fuzzer

---
+++ Fuzz/fuzz_abseil_strings.cc
@@ -0,0 +1,8 @@
+#include <string>
+
+#include "/usr/local/google/home/kabeerseth/github/absl/strings/str_join.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+	cout << "test" << endl;
+	return 0;
+}
