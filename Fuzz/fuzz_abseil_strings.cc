49db2d418c640c9f2e0c49c050a33b417aa7276c
Kabeer Seth <kabeerseth@google.com>
2020-06-09 13:06:32 +0000

    Test fuzzer string split and join

--- Fuzz/fuzz_abseil_strings.cc
+++ Fuzz/fuzz_abseil_strings.cc
@@ -4,6 +4,9 @@
 #include "../absl/strings/str_split.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-	cout << "test" << endl;
+	std::string str(reinterpret_cast<const char*>(data), size);
+	std::vector<std::string> v = absl::StrSplit(str, ',');
+	std::string str = absl::StrJoin(v, "-");
+
 	return 0;
 }
