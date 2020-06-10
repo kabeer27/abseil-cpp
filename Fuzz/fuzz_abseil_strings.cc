d4f8360ea155dc031d210605d9f7305a7fb53c42
Kabeer Seth <kabeerseth@google.com>
2020-06-10 07:27:21 +0000

    Upgrading string escape fuzzer

--- Fuzz/fuzz_abseil_strings.cc
+++ Fuzz/fuzz_abseil_strings.cc
@@ -5,8 +5,9 @@
 #include "../absl/strings/numbers.h"
 #include "../absl/strings/str_cat.h"
 
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-	size_t min_length = 
+	size_t min_length = 13 * sizeof()
 	std::string str (reinterpret_cast<const char*>(data), size);
 	std::vector<std::string> v = absl::StrSplit(str, ',');
 	str = absl::StrJoin(v, ",");
--- Fuzz/string_escape_fuzzer.cc
+++ Fuzz/string_escape_fuzzer.cc
@@ -4,8 +4,29 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 	std::string str (reinterpret_cast<const char*>(data), size);
-	std::string escaped = absl::CHexEscape(str);
-	std::string unescaped;
+	std::string escaped, unescaped;
+	escaped = absl::CHexEscape(str);
 	absl::CUnescape(escaped, &unescaped);
+	
+	escaped = absl::CEscape(str);
+	absl::CUnescape(escaped, &unescaped);
+	
+	escaped = absl::CUtf8SafeEscape(str);
+	abs::CUnescape(escaped, &unescaped);
+	
+	escaped = absl::Utf8SafeCHexEscape(str);
+	abs::Unescape(escaped, &unescaped);
+	
+	std::string encoded, decoded;
+	abs::Base64Escape(str, &encoded);
+	abs::Base64Unescape(encoded, &decoded);
+
+	absl::WebSafeBase64Escape(str, &encoded);
+	absl::WebSafeBase64UnEscape(encoded, &decoded);
+
+	std::string hex_result, bytes_result;
+	hex_result = absl::BytesToHexString(str);
+	bytes_result = absl::HexStringToBytes(hex_result);
+
 	return 0;
 }
