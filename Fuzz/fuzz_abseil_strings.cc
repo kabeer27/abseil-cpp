7285145e0724927991aceba5097449cc8d635e0e
Kabeer Seth <kabeerseth@google.com>
2020-06-10 05:53:47 +0000

    Adding string_escape_fuzzer

--- Fuzz/fuzz_abseil_strings.cc
+++ Fuzz/fuzz_abseil_strings.cc
@@ -2,11 +2,14 @@
 
 #include "../absl/strings/str_join.h"
 #include "../absl/strings/str_split.h"
+#include "../absl/strings/numbers.h"
+#include "../absl/strings/str_cat.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+	size_t min_length = 
 	std::string str (reinterpret_cast<const char*>(data), size);
 	std::vector<std::string> v = absl::StrSplit(str, ',');
-	str = absl::StrJoin(v, "-");
+	str = absl::StrJoin(v, ",");
 
 	return 0;
 }
---
+++ Fuzz/string_escape_fuzzer.cc
@@ -0,0 +1,11 @@
+#include <string> 
+
+#include <../absl/strings/escaping.h>
+
+extern "C" LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+	std::string str (reinterpret_cast<const char*>(data), size);
+	std::string escaped = absl::CHexEscape(str);
+	std::string unescaped;
+	absl::CUnescape(escaped, &unescaped);
+	return 0;
+}
